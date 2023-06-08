87a45903cb63b1b7959c5c717f98d06493730cae
Luca1056 <luca15.masiero@gmail.com>
2023-06-08 17:59:29 +0200

    Modify vehicle struct

--- src/struct.h
+++ src/struct.h
@@ -13,19 +13,19 @@ typedef struct vehicle_s {
     struct vehicle_s * next;     // pointer to next vehicle of the list
 } vehicle;
 
-struct station_data {
+typedef struct station_data_s {
     uint32_t dist;               // Distance from the beginning of the highway
     uint32_t max_autonomy;       // maximum autonomy between the entire fleet of vehicles
-    uint32_t currShortestPath;   // The shortest path found until this moment (Dijkstra)
+    uint32_t shortest_path;   // The shortest path found until this moment (Dijkstra)
     vehicle *fleet;              // RB-Tree of vehicles
-    struct station_data *prev;   // Previous station in the shortest path (Dijkstra)
-};
+    struct station_data_s *prev;   // Previous station in the shortest path (Dijkstra)
+}station_data;
 
 typedef struct station_node_s{
     uint8_t color;
     struct station_node_s * parent;
     struct station_node_s * left;
-    struct station_data station;
+    station_data station;
     struct station_node_s * right;
 } service_station;
 
